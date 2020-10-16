059247d1a0bdca3fd3763c7b471e6c70ff115e0b
James <tuh01663@temple.edu.com>
2020-10-16 14:46:03 -0400

    Added files from other modules
    Added scripts for camera and web portal to their own module.

---
+++ Camera_Module/Camera.py
@@ -0,0 +1,31 @@
+import cv2 as cv
+import numpy as np
+
+
+def save_image(img: np.ndarray, path):
+    cv.imwrite(path, img)
+
+
+def show_image(img: np.ndarray):
+    cv.imshow('frame', img)
+
+
+class Camera:
+    def __init__(self):
+        self.cam = cv.VideoCapture(0)
+        self.frame_size = (800, 550)
+        self.video = cv.VideoWriter('static/video.mp4', 0, 30, self.frame_size)
+
+    def __del__(self):
+        self.cam.release()
+        self.video.release()
+        cv.destroyAllWindows()
+
+    # Grabs frame from passed VideoCapture object (usb camera)
+    def grab_frame(self) -> np.ndarray:
+        ret, frame = self.cam.read()
+        frame = cv.resize(frame, self.frame_size, interpolation=cv.INTER_NEAREST)
+        norm = np.zeros(self.frame_size)
+        norm = cv.normalize(frame, norm, 0, 255, cv.NORM_MINMAX)
+        self.video.write(norm)
+        return norm
---
+++ Camera_Module/CameraDriver.py
@@ -0,0 +1,65 @@
+from multiprocessing.connection import PipeConnection
+
+from ModuleCommunicationHandler.ModuleMessage import ModuleMessage
+
+_Minfo = {
+    "version": 1,
+    "name": "Example Module",
+    "entry_point": -1,
+    # The code used to issue messages and establish pipes to the module
+    "message_code": "EMM",
+}
+
+
+# Processes any messages left on the queue
+def __proc_message__(conn: PipeConnection):
+    # if we receive a message on the connection act on it
+    if conn.poll():
+        m = conn.recv()
+        # Verify that the sender used the proper data type to send the message
+        if isinstance(m, ModuleMessage):
+            # Check if a message code exists for the given module
+            ### HANDLE MESSAGES HERE ###
+            print("User IO: ", m.message)
+        else:
+            print("Error! received unknown object as a message!")
+
+
+# This contains the actual operation of the module which will be run every time
+def __operation__():
+    ### ADD MODULE OPERATIONS HERE ###
+    # Let the world know we are loading a new object
+    img = []
+    setup_message = ModuleMessage("WPM",
+                                  "ready",
+                                  img)
+    pass
+
+
+# Runs the modules functionality
+def __load__(conn: PipeConnection):
+    # Let the world know we are loading a new object
+    setup_message = ModuleMessage("HIO",
+                                  "loading",
+                                  "Loading " + _Minfo["name"] + "...")
+    conn.send(setup_message)
+
+    # Let the world know we are loading a new object
+    setup_message = ModuleMessage("HIO",
+                                  "ready",
+                                  _Minfo["name"] + " done loading!")
+    conn.send(setup_message)
+    running = True
+    # While we are running do operations
+    while running:
+        __operation__()
+        __proc_message__(conn)
+
+
+# Set the entry point function
+_Minfo["entry_point"] = __load__
+
+
+# Returns a dictionary containing information that describes the module
+def __module_info__():
+    return _Minfo
