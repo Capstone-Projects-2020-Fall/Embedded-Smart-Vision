c3fa9c70e62bf64f63d85f82aeab44000d736fcf
James <tuh01663@temple.edu.com>
2020-10-20 21:44:34 -0400

    Adds face detection
    Adds face detection from camera branch; done because merge conflicts made it difficult to make these changes using a pull request

--- Camera_Module/Camera.py
+++ Camera_Module/Camera.py
@@ -1,14 +1,26 @@
-import cv2 as cv
-import numpy as np
 import os
 
+import cv2 as cv
+import numpy as np
 
-def save_image(img: np.ndarray, path):
+
+def save_image(img, path):
     cv.imwrite(path, img)
 
 
-def show_image(img: np.ndarray):
+def show_image(img):
     cv.imshow('frame', img)
+
+
+def face_detection(cascade, image):
+    image_copy = image.copy()
+    grayscale = cv.cvtColor(image_copy, cv.COLOR_BGR2GRAY)
+    faces = cascade.detectMultiScale(grayscale, scaleFactor=1.1, minNeighbors=5)
+
+    for (x, y, w, h) in faces:
+        cv.rectangle(image_copy, (x, y), (x + w, y + h), (0, 255, 0), 15)
+    crop_image = image_copy[y: y + h, x: x + w]
+    return crop_image
 
 
 class Camera:
@@ -24,7 +36,7 @@ class Camera:
         cv.destroyAllWindows()
 
     # Grabs frame from passed VideoCapture object (usb camera)
-    def grab_frame(self) -> np.ndarray:
+    def grab_frame(self):
         ret, frame = self.cam.read()
         frame = cv.resize(frame, self.frame_size, interpolation=cv.INTER_NEAREST)
         norm = np.zeros(self.frame_size)
