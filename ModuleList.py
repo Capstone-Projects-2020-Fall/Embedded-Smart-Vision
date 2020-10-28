20286eb4602ca0a083bca96af1eb880b59fe7918
James <tuh01663@temple.edu.com>
2020-10-27 23:44:03 -0400

    Merge Conflict Changes

--- ModuleList.py
+++ ModuleList.py
@@ -1,11 +1,9 @@
 from ExampleModule import ExampleModule
 from AnotherModule import AnotherModule
 from HostUserIOModule import HostUserIOModule
-from Webportal import Webportal
 
 module_list = [
-    ExampleModule.__module_info__(),
-    # AnotherModule.__module_info__(),
-    # HostUserIOModule.__module_info__(),
-    # Webportal.__module_info__()
+    AnotherModule.__module_info__(),
+    HostUserIOModule.__module_info__(),
+    Camera_Module.__module_info__()
 ]
