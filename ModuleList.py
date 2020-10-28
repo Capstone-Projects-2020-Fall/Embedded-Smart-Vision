6d5bbbb39da275d79c743809fdd45465ddad6e21
James <tuh01663@temple.edu.com>
2020-10-27 23:46:01 -0400

    More Merge Conflict

--- ModuleList.py
+++
@@ -1,9 +0,0 @@
-from ExampleModule import ExampleModule
-from AnotherModule import AnotherModule
-from HostUserIOModule import HostUserIOModule
-
-module_list = [
-    AnotherModule.__module_info__(),
-    HostUserIOModule.__module_info__(),
-    Camera_Module.__module_info__()
-]
