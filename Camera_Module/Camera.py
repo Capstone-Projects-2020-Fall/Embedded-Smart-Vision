78b6853015c4da4bca6519b71b107ec56e2e8f90
jimmyahurd <tuh01663@temple.edu>
2020-11-11 00:52:48 -0500

    Frames only classified when motion is detected

--- Camera_Module/Camera.py
+++ Camera_Module/Camera.py
@@ -41,5 +41,5 @@ class Camera:
         frame = cv.resize(frame, self.frame_size, interpolation=cv.INTER_NEAREST)
         norm = np.zeros(self.frame_size)
         norm = cv.normalize(frame, norm, 0, 255, cv.NORM_MINMAX)
-        found = detect_face(self.cascade, norm)
-        return norm, found
+        #found = detect_face(self.cascade, norm)
+        return norm
--- Classification_Module/ClassificationDriver.py
+++ Classification_Module/ClassificationDriver.py
@@ -1,3 +1,6 @@
+import os
+from datetime import datetime
+
 from ModuleMessage import ModuleMessage
 from Classification_Module.Classifier import Classifier
 import cv2 as cv
@@ -12,6 +15,11 @@ _Minfo = {
     # The code used to issue messages and establish pipes to the module
     "message_code": "IPM",
 }
+
+recording = False
+video_count = 0
+last_found = datetime.now()
+frames = list()
 
 
 # Processes any messages left on the queue
@@ -31,11 +39,67 @@ def __proc_message__(conn):
                 print('done classifying!')
             if m.target == 'IPM' and m.tag == 'New Frame':
                 frame = m.message
-                name = classifier.classify(frame)
-                classified_message = ModuleMessage("CM", "Classification", name)
-                conn.send(classified_message)
+                names = classifier.classify(frame)
+                if len(names) > 0:
+                    print(names)
         else:
             print("Error! received unknown object as a message!")
+
+"""
+def start_recording():
+    global recording, frames, last_found
+    if found:
+        # Check who is in the frame
+        classify_message = ModuleMessage("IPM", "New Frame", frame)
+        conn.send(classify_message)
+        # Add frame to video and document time face was last found
+        last_found = datetime.now()
+        frames.append(frame)
+        faces.append(frame)
+        # Start Recording if we aren't already
+        if not recording:
+            recording = True
+            print('starting recording')
+    else:
+        if recording:
+            # Add frame because we're recording
+            frames.append(frame)
+            current_time = datetime.now()
+            delta = current_time - last_found
+            # Face has not been present for too long, sto stop recording
+            if delta.seconds > 3:
+                frame_copy = frames.copy()
+                face_copy = faces.copy()
+                frames.clear()
+                faces.clear()
+                start_new_thread(make_video, (frame_copy, face_copy, conn, ))
+                recording = False
+
+
+def stop_recording():
+    global recording, frames, last_found
+"""
+
+
+def make_video(frames: list, faces: list, conn):
+    print('Making Video')
+    global video_count
+    video_count += 1
+    path = os.path.join(os.getcwd(), 'Videos', 'video%d.mp4' % video_count)
+    video = cv.VideoWriter(path, cv.VideoWriter_fourcc('a','v','c','1'), 10, (800, 550))
+    for frame in frames:
+        video.write(frame)
+    video.release()
+
+    #Upload video to database
+    path = os.path.basename(path)
+    tag = ('face',)
+    add_video_message = ModuleMessage("WPM", "New Video Path", (os.path.basename(path), tag))
+    conn.send(add_video_message)
+
+    #Send faces to classify
+    video_message = ModuleMessage('IPM', 'video', faces)
+    conn.send(video_message)
 
 
 # This contains the actual operation of the module which will be run every time
@@ -46,7 +110,10 @@ def __operation__():
 
 # Runs the modules functionality
 def __load__(conn):
-    
+    # Make Directory for Videos
+    path = os.path.join(os.getcwd(), 'Videos/')
+    if not os.path.isdir(path):
+        os.mkdir(path)
 
     running = True
     # While we are running do operations
