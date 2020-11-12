d255088abe35997af3a9b9257d966194482d3d72
jimmyahurd <tuh01663@temple.edu>
2020-11-12 17:14:53 -0500

    LED lights up when person is found

--- ModuleList.py
+++ ModuleList.py
@@ -1,10 +1,12 @@
 from Camera_Module import CameraDriver
 from Classification_Module import ClassificationDriver
 from Webportal_Module import WebDriver
+from Action_Module import ActionDriver
 
 
 module_list = [
     CameraDriver.__module_info__(),
     ClassificationDriver.__module_info__(),
-    WebDriver.__module_info__()
+    WebDriver.__module_info__(),
+    ActionDriver.__module_info__()
 ]
